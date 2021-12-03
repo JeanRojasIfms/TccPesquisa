feb458922cae8f2fdb38b8c5a27b569829b953ac
JeanRojasIfms <jean.rojas@ifms.edu.br>
2021-12-03 16:19:59 -0400

    personalizando o retorno da validação

--- src/main/java/br/edu/ifms/pesquisa/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/br/edu/ifms/pesquisa/resources/exception/ResourceExceptionHandler.java
@@ -3,6 +3,8 @@ package br.edu.ifms.pesquisa.resources.exception;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -15,17 +17,32 @@ import br.edu.ifms.pesquisa.service.exception.ObjectNotFoundException;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	// o método recebe a exceção q já explodiu
-		// HttpServletRequest request --> informações da requisição
-		@ExceptionHandler(ObjectNotFoundException.class) // anotação indica q é um tratador desse tipo de exceção
-		public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
-			StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	// HttpServletRequest request --> informações da requisição
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
+				"Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"Integridade de dados", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+
+		ValidationError err = new ValidationError(System.currentTimeMillis(), HttpStatus.UNPROCESSABLE_ENTITY.value(),
+				"Erro de validação", e.getMessage(), request.getRequestURI());
+		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
 		}
-		
-		@ExceptionHandler(DataIntegrityException.class) // anotação indica q é um tratador desse tipo de exceção
-		public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
-			StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
-		}
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(err);
+	}
 
 }
